7dedf0eb45b234bf1c260efa1397f475f5228d78
Matias Jalava <matias@elm.fi>
2021-03-29 12:00:32 +0300

    mongodb

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-// server.js
 import {ApolloServer} from 'apollo-server-express';
 import schemas from './schemas/index.js';
 import resolvers from './resolvers/index.js';
 import express from 'express';
+import dotenv from 'dotenv';
+import connectMongo from './db/db.js';
+
+dotenv.config();
 
 (async () => {
-   try {
-      const server = new ApolloServer({
-         typeDefs: schemas,
-         resolvers,
-      });
-   
-       const app = express();
-   
-       server.applyMiddleware({app});
-   
-       app.listen({port: 3000}, () =>
-           console.log(
-               `👌 Server ready at http://localhost:3000${server.graphqlPath}`),
-       );
-   } catch (e) {
-      console.log('server error: ' + e.message);
-   }
+  try {
+    const conn = await connectMongo();
+    if (conn) {
+      console.log('Connected successfully.');
+    }
+
+    const server = new ApolloServer({
+      typeDefs: schemas,
+      resolvers,
+    });
+
+    const app = express();
+
+    server.applyMiddleware({app});
+
+    app.listen({port: 3000}, () =>
+        console.log(
+            `🚀 Server ready at http://localhost:3000${server.graphqlPath}`),
+    );
+  } catch (e) {
+    console.log('server error: ' + e.message);
+  }
 })();
