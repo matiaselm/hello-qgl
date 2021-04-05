1d0f11c55911b2cf8031ef2c01418ae78f985c82
Matias Jalava <matias@elm.fi>
2021-04-05 17:53:52 +0300

    removed unessessary logs

--- resolvers/levelResolver.js
+++ resolvers/levelResolver.js
@@ -4,7 +4,7 @@ import Level from '../models/level.js';
 export default {
   Connection: {
     Level(parent) {
-      console.log('levelType', parent);
+      // console.log('levelType', parent);
       return Level.findById(parent.LevelID);
     },
   },
